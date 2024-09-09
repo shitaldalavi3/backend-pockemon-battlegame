31c4a85572b754fb303805d649eefad6af66fb47
shital Dalavi <shital.dalavi3@gmail.com>
2024-09-09 17:43:51 +0200

    create schema of LeaderBoard with username,score,date

--- schemas/LeaderBoard.js
+++ schemas/LeaderBoard.js
@@ -4,10 +4,11 @@ import Joi from "joi";
 const leaderBoardSchema = new mongoose.Schema({
   username: Joi.string().min(3).max(30).required(),
   score: Joi.number.required(),
-  date: {
+  /*date: {
     type: Date,
     default: Date.now, // Set the default value to the current timestamp
-  },
+  },*/
+  date: Joi.date().required(),
 });
 
 module.exports = mongoose.model(leaderBoardSchema);
