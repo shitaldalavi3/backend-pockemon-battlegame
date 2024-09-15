5b7157de0d7a730bc14383a75ca6f8a0ed539555
shital Dalavi <shital.dalavi3@gmail.com>
2024-09-15 23:42:08 +0200

    add user functionality in controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,40 +1,40 @@
 const User = require("../schemas/User");
-const jwt = require("jsonwebtoken");
+// const jwt = require("jsonwebtoken");
 
-const createToken = (_id) => {
-  return jwt.sign({ _id }, process.env.SECRET, { expiresIn: "1d" });
-};
+// const createToken = (_id) => {
+//   return jwt.sign({ _id }, process.env.SECRET, { expiresIn: "1d" });
+// };
 
-// login user
-const loginUser = async (req, res) => {
-  const { email, password } = req.body;
+// // login user
+// const loginUser = async (req, res) => {
+//   const { email, password } = req.body;
 
-  try {
-    const user = await User.login(email, password);
+//   try {
+//     const user = await User.login(email, password);
 
-    //create token
-    const token = createToken(user._id);
+//     //create token
+//     const token = createToken(user._id);
 
-    res.status(200).json({ email, token });
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-};
+//     res.status(200).json({ email, token });
+//   } catch (error) {
+//     res.status(400).json({ error: error.message });
+//   }
+// };
 
-// register user
-const registerUser = async (req, res) => {
-  const { email, password } = req.body;
+// // register user
+// const registerUser = async (req, res) => {
+//   const { email, password } = req.body;
 
-  try {
-    const user = await User.signup(email, password);
-    //create token
-    const token = createToken(user._id);
-    res.status(200).json({ email, token });
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-};
-const User = require("../schemas/User");
+//   try {
+//     const user = await User.signup(email, password);
+//     //create token
+//     const token = createToken(user._id);
+//     res.status(200).json({ email, token });
+//   } catch (error) {
+//     res.status(400).json({ error: error.message });
+//   }
+// };
+// const User = require("../schemas/User");
 
 // Get all users
 const getAllUsers = async (req, res) => {
@@ -121,8 +121,6 @@ const deleteUser = async (req, res) => {
 };
 
 module.exports = {
-  loginUser,
-  registerUser,
   getAllUsers,
   getOneUser,
   createUser,
