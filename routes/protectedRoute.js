24078328df817934cb644c5271363b0d30fc303c
shital Dalavi <shital.dalavi3@gmail.com>
2024-09-15 20:50:03 +0200

    add Using Protected Routes

---
+++ routes/protectedRoute.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const checkAuth = require("../middleware/auth");
+
+// A protected route is define here
+router.get("/profile", Auth, (req, res) => {
+  // Access user data through req.userData
+  res.json({ message: "You are authenticated" });
+});
+
+module.exports = router;
