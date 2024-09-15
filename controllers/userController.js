c09460fc8331a0df1c514ef53d6945376110ace4
shital Dalavi <shital.dalavi3@gmail.com>
2024-09-15 23:25:27 +0200

    add functionality in userConroller  getAllUsers,   getOneUser,   createUser,   updateUser,   deleteUser

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,4 @@
-const User = require("../models/User");
+const User = require("../schemas/User");
 const jwt = require("jsonwebtoken");
 
 const createToken = (_id) => {
@@ -34,5 +34,98 @@ const registerUser = async (req, res) => {
     res.status(400).json({ error: error.message });
   }
 };
+const User = require("../schemas/User");
 
-module.exports = { loginUser, registerUser };
+// Get all users
+const getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    if (!users.length) {
+      return res
+        .status(200)
+        .json({ message: "No users found in the database" });
+    } else {
+      return res.status(200).json(users);
+    }
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+};
+
+// Get one user by ID
+const getOneUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findById(id);
+    if (user) {
+      return res.status(200).json({ user });
+    }
+    return res.status(404).json({ message: "User not found" });
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+};
+
+// Create a new user
+const createUser = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    const newUser = new User({ username, email, password });
+
+    const savedUser = await newUser.save();
+    return res.status(201).json(savedUser);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+};
+
+// Update an existing user by ID
+const updateUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { username, email, password } = req.body;
+
+    const updatedUser = await User.findByIdAndUpdate(
+      id,
+      { username, email, password },
+      { new: true }
+    );
+
+    if (!updatedUser) {
+      return res.status(404).json({ message: "User not found" });
+    } else {
+      res.status(200).json({ msg: "user updated successfully", updatedUser });
+    }
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+};
+
+// Delete a user by ID
+const deleteUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const deletedUser = await User.findByIdAndDelete(id);
+
+    if (!deletedUser) {
+      return res.status(404).json({ message: "User not found" });
+    } else {
+      return res
+        .status(200)
+        .json({ message: "User deleted successfully", deletedUser });
+    }
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+};
+
+module.exports = {
+  loginUser,
+  registerUser,
+  getAllUsers,
+  getOneUser,
+  createUser,
+  updateUser,
+  deleteUser,
+};
