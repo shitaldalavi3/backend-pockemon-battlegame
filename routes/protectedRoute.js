2be2bde838258da1afae57853cb0d16ae9e2f3c3
shital Dalavi <shital.dalavi3@gmail.com>
2024-09-15 23:00:22 +0200

    add checkAuth instead of Auth in get method of Profile link

--- routes/protectedRoute.js
+++ routes/protectedRoute.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const checkAuth = require("../middleware/auth");
 
 // A protected route is define here
-router.get("/profile", Auth, (req, res) => {
+router.get("/profile", checkAuth, (req, res) => {
   // Access user data through req.userData
   res.json({ message: "You are authenticated" });
 });
